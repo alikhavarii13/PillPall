5d2eb558611a7302bf85c87c520ed638dd1c46ec
alikhavarii13 <sdyarash@gmail.com>
2025-10-26 23:12:49 +0330

    Add dotenv support for environment variables and update dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:health_reminder/core/shared/app_theme.dart';
 import 'package:health_reminder/feature/ui/auth/view/sign_in_screen.dart';
-import 'package:health_reminder/feature/ui/home/view/screen/home_screen.dart';
 import 'package:health_reminder/notification_helper.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await dotenv.load(fileName: "assets/.env");
+  } catch (e) {
+    throw Exception('Error loading .env file: $e');
+  }
   await Supabase.initialize(
-    url: 'https://fjhqdhcmmofhgcldfxgb.supabase.co',
-    anonKey: 'sb_publishable_NmBk2sYi81tAXqoJ3ckKqA_ekeMapHy',
+    url: dotenv.env['BASE_URL'] ?? 'default_url',
+    anonKey: dotenv.env['API_KEY'] ?? 'default_key',
   );
   await NotificationHelper().initializeNotification();
   runApp(const ProviderScope(child: PillPallApp()));
