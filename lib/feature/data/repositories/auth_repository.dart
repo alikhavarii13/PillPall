92ee5520b5849a4931e62ff73c32476a9b5c1832
alikhavarii13 <sdyarash@gmail.com>
2025-10-28 23:37:38 +0330

    nothing

--- lib/feature/data/repositories/auth_repository.dart
+++ lib/feature/data/repositories/auth_repository.dart
@@ -1,7 +1,7 @@
 abstract class AuthRepository {
   Future<bool> get isAuthenticated;
 
-  Future<dynamic> login({required String email, required String password});
+  Future<void> login({required String email, required String password});
 
   Future<void> logout();
 }
