1835fa8bc82afd2214d80add9a2179ee90c322bd
alikhavarii13 <sdyarash@gmail.com>
2025-10-30 22:42:55 +0330

    nothing

--- lib/feature/data/repositories/auth_repository.dart
+++ lib/feature/data/repositories/auth_repository.dart
@@ -1,7 +1,9 @@
+import 'package:health_reminder/feature/data/service/api/model/login_request/login_request_model.dart';
+
 abstract class AuthRepository {
   Future<bool> get isAuthenticated;
 
-  Future<void> login({required String email, required String password});
+  Future<void> login({required LoginRequestModel model});
 
   Future<void> logout();
 }
