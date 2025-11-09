9915e42823de11013ac6fca94c62a927c55ec1b5
alikhavarii13 <sdyarash@gmail.com>
2025-11-09 23:08:08 +0330

    Refactor TokenNotifier methods for clarity

--- lib/feature/auth/api_client.dart
+++ lib/feature/auth/api_client.dart
@@ -20,42 +20,37 @@ class TokenNotifier extends StateNotifier<String?> {
 
   Future<String?> getToken() async {
     SharedPreferences pref = await SharedPreferences.getInstance();
-
-    return pref.getString(_tokenKey);
+    state = pref.getString(_tokenKey);
+    return state;
   }
 
   Future<bool> saveString(String? token) async {
     SharedPreferences pref = await SharedPreferences.getInstance();
+    state = token;
+    return pref.setString(_tokenKey, state!);
+  }
 
-    return pref.setString(_tokenKey, token!);
+  Future<void> logout() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.remove(_tokenKey);
+    state = null;
   }
 }
-// class AuthApiClient {
-//   AuthApiClient({String? host, int? port, HttpClient Function()? clientFactory})
-//     : _host = host ?? 'localhost',
-//       _port = port ?? 8080,
-//       _clientFactory = clientFactory ?? HttpClient.new;
 
-//   final String _host;
-//   final int _port;
-//   final HttpClient Function() _clientFactory;
+final tokenProvider = StateNotifierProvider<TokenNotifier, String?>(
+  (ref) => TokenNotifier(),
+);
 
-//   Future<Result<LoginResponse>> login(LoginRequest loginRequest) async {
-//     final client = _clientFactory();
-//     try {
-//       final request = await client.post(_host, _port, '/login');
-//       request.write(jsonEncode(loginRequest));
-//       final response = await request.close();
-//       if (response.statusCode == 200) {
-//         final stringData = await response.transform(utf8.decoder).join();
-//         return Result.ok(LoginResponse.fromJson(jsonDecode(stringData)));
-//       } else {
-//         return const Result.error(HttpException("Login error"));
-//       }
-//     } on Exception catch (error) {
-//       return Result.error(error);
-//     } finally {
-//       client.close();
-//     }
-//   }
-// }
+final dioProvider = Provider<Dio>((ref) {
+  final token = ref.watch(tokenProvider);
+  final dio = Dio(BaseOptions(baseUrl: ''));
+  dio.interceptors.add(
+    InterceptorsWrapper(
+      onRequest: (options, handler) {
+        options.headers['Authorization'] = token;
+        handler.next(options);
+      },
+    ),
+  );
+  return dio;
+});
